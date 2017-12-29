2a2578a79b5d711c7dd4effe0644dd605bd94b04
Gianluca Arbezzano <gianarb92@gmail.com>
2017-12-29 17:33:17 +0100

    refactored spec and removed abstractions

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -1,18 +1,16 @@
 package operator
 
 import (
-	"fmt"
 	"log"
 	"sync"
-	"time"
 
-	"github.com/gianarb/influxdb-operator/pkg/k8sutil"
-	"k8s.io/api/core/v1"
+	"github.com/gianarb/influxdb-operator/pkg/client/tick/v1alpha1"
+	extensionsobj "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	"k8s.io/apimachinery/pkg/fields"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -20,8 +18,8 @@ var (
 	VERSION       = "0.0.0.dev"
 	TPRGroup      = "gianarb.com"
 	TPRVersion    = "v1beta1"
-	TPRName       = "tick"
-	TPRNamePlural = "ticks"
+	TPRName       = "tickCluster"
+	TPRNamePlural = "tickClusters"
 )
 
 type Options struct {
@@ -30,8 +28,9 @@ type Options struct {
 
 type InfluxDBOperator struct {
 	Options
-	k8swrap       k8sutil.K8sWrap
-	tickTprClient *rest.RESTClient
+	kubeClient *kubernetes.Clientset
+	tickCs     *rest.RESTClient
+	clientSet  clientset.Interface
 }
 
 func New(options Options) *InfluxDBOperator {
@@ -48,72 +47,48 @@ func New(options Options) *InfluxDBOperator {
 	kubeClient := kubernetes.NewForConfigOrDie(config)
 	cs := clientset.NewForConfigOrDie(config)
 
-	spec := TICKSpec{}
-	specList := TICKSpecList{}
-	tickTrpClient, _, err := k8sutil.NewThridPartyResourceClient(config, TPRGroup, TPRVersion, &spec, &specList)
+	rest, _, err := v1alpha1.NewForConfig(config)
 	if err != nil {
 		log.Fatalf("Couldn't get Tick trd client: %s", err)
 	}
 
-	return &InfluxDBOperator{
-		Options:       options,
-		k8swrap:       k8sutil.NewK8sWrap(kubeClient, cs),
-		tickTprClient: tickTrpClient,
+	operator := &InfluxDBOperator{
+		Options:    options,
+		tickCs:     rest,
+		kubeClient: kubeClient,
+		clientSet:  cs,
 	}
+
+	return operator
 }
 
 func (operator *InfluxDBOperator) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
 	log.Printf("TICK OSS operator started. Version %v\n", VERSION)
-	operator.k8swrap.RegisterThridPartyResource(TPRGroup, TPRName, TPRNamePlural, TPRVersion)
-	events, errChan := operator.processTickEvents(stopCh)
-	go func(events <-chan *TICKSpec) {
-		for {
-			select {
-			case event := <-events:
-				fmt.Printf("RUN %+v\n", event)
-			case err := <-errChan:
-				fmt.Printf("RUN %s\n", err)
-			case <-stopCh:
-				wg.Done()
-				return
-			}
+
+	influxCrd := extensionsobj.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "influxdbs" + "." + "gianarb.com",
+			Labels: map[string]string{},
+		},
+		Spec: extensionsobj.CustomResourceDefinitionSpec{
+			Group:   "gianarb.com",
+			Version: "alphav1",
+			Scope:   extensionsobj.NamespaceScoped,
+			Names: extensionsobj.CustomResourceDefinitionNames{
+				Plural: "influxdbs",
+				Kind:   "influxdb",
+			},
+		},
+	}
+
+	crds := []*extensionsobj.CustomResourceDefinition{
+		&influxCrd,
+	}
+	crdClient := operator.clientSet.ApiextensionsV1beta1().CustomResourceDefinitions()
+	for _, crd := range crds {
+		if _, err := crdClient.Create(crd); err != nil && !apierrors.IsAlreadyExists(err) {
+			panic(err)
 		}
-	}(events)
-}
-
-func (operator *InfluxDBOperator) processTickEvents(stopCh <-chan struct{}) (<-chan *TICKSpec, <-chan error) {
-	events := make(chan *TICKSpec)
-	errc := make(chan error, 1)
-	source := cache.NewListWatchFromClient(operator.tickTprClient, TPRNamePlural, v1.NamespaceAll, fields.Everything())
-	createAddHandler := func(obj interface{}) {
-		event := obj.(*TICKSpec)
-		event.Type = "ADDED"
-		events <- event
+		log.Printf("CRD created %s", crd.Spec.Names.Kind)
 	}
-
-	deleteHandler := func(obj interface{}) {
-		event := obj.(*TICKSpec)
-		event.Type = "DELETED"
-		events <- event
-	}
-
-	updateHandler := func(old interface{}, obj interface{}) {
-		event := obj.(*TICKSpec)
-		event.Type = "MODIFIED"
-		events <- event
-	}
-
-	_, controller := cache.NewInformer(
-		source,
-		&TICKSpec{},
-		time.Minute*60,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc:    createAddHandler,
-			UpdateFunc: updateHandler,
-			DeleteFunc: deleteHandler,
-		})
-
-	go controller.Run(stopCh)
-
-	return events, errc
 }
